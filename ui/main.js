163a1b204a15e4abfd41c90eb3d5fb08e95e220b
muruga121 <deepdurai02@gmail.com>
2016-09-27 12:53:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,14 +28,29 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-  //Make a request to the server and send a name
-  
-  //Capture a list of name and render it as a list
-  var names = ['name1','name2','name3','name4']; 
+     //Make a request to the counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it as a variable
+    request.onreadystatechange = function () {
+      if(request.readystate === XMLHttpRequest.DONE);
+      //Take Some action
+      if(request.status === 200) {
+        //Capture a list of name and render it as a list
+  var names = request.responseText;
+  names = JSON.parse(names);
   var list = '';
 }
   for(var i=0;i<names.length;i++) {
       list +- '<li>' + names[i] + </li> ;
       var ul = document.getElementById('namelist');
       ul.innerHTML = list;
-};    
+}   
+}
+      //Not done yet
+};
+    //Make a request
+   request.open('GET','http://muruga121.imad.hasura-app.io/submit-name?name=' +name,true);
+   request.send(null);
+  
+  
