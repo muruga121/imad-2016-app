9822fa2b178cbbecff9e7724e81657d5e89a9ffe
muruga121 <deepdurai02@gmail.com>
2016-10-12 11:41:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-
 var articles = { 
 'article-one': {
     title: 'Article One | Murugaprasaad',
     heading: 'Article one',
     Date: 'Sep 5,2016',
-    content:`
+    content:
+	`
     <p>
                Hi Everyone! I'm Murugaprasaad the developer of this webapp....
     </p>
     <p>
                In the next few pages , I will be introducing about myself.
-    </p>
+    </p>`
     
 },    
 'Personal Info': {
@@ -27,9 +27,8 @@ var articles = {
     content:`
     <p>
     This is the content for my second article.This is the content for my second article.
-    </p>
+    </p>`
 },
-
 'Hobbies': {
     title: 'Hobbies | Murugaprasaad',
     heading: 'Hobbies',
@@ -37,8 +36,8 @@ var articles = {
     content:`
     <p>
     This is the content for my third article.This is the content for my third article.
-    </p>
-;
+    </p>`
+}
 };
 
 function createTemplate (data) {
@@ -76,7 +75,7 @@ ${date}
 </html>
 `;
 return htmlTemplate;
-}
+};
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -84,41 +83,37 @@ app.get('/', function (req, res) {
 
 var counter = 0;
 app.get('/counter', function(req,res) {
-    counter = counter + 1;
-    res.send(counter.toString());
-    });
-    
-
-app.get('article-one', function(req,res) {
-    res.send(createTemplate(articleOne));
-});   
-
-app.get('article-two', function(req,res) {
-    res.send('Article two requested and will be served here');
+counter = counter + 1;
+res.send(counter.toString());
 });
 
-app.get('article-three', function(req,res) {
-    res.send('Article three requested and will be served here');
+app.get('/article-one', function(req,res) {
+res.send(createTemplate(articles['article-one']));
+});
+
+app.get('/article-two', function(req,res) {
+res.send('Article two requested and will be served here');
+});
+
+app.get('/article-three', function(req,res) {
+res.send('Article three requested and will be served here');
 });
 app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var names = [];
 app.get('/submit-name', function(req,res) { // URL: /submit-name?name=xxxxx
-  //Get the name from the request
-  var name = req.params.name;
-  
-  names.push(name);
-  //JSON:Javascript Object Notation
-  
-  res.send(JSON.stringify(names)); 
-};
+var name = req.params.name;
+names.push(name);
+//JSON:Javascript Object Notation
 
+res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
