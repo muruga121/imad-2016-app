378a04afecc1ce1c4c72cf818131b603e7d496ad
muruga121 <deepdurai02@gmail.com>
2016-10-23 07:33:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,6 +81,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+app.get('/test-db', function (req,res) {
+  //Make a select request
+  //Return a response with the result
+});
+
+
 var counter = 0;
 app.get('/counter', function(req,res) {
 counter = counter + 1;
